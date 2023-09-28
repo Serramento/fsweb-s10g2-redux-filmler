534e9264849d74360aa4492c5a169341d193daed
Serramento <134259414+Serramento@users.noreply.github.com>
2023-09-28 11:30:02 +0300

    S10G2 tamamlandı

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import MovieListItem from './MovieListItem';
 import MovieListFooter from './MovieListFooter';
 
 const MovieList = (props) => {
-  const movies = [];
+  
+  const movies = props.movies;
 
   return (
     <div className="flex-1">
@@ -33,4 +35,8 @@ const MovieList = (props) => {
   );
 }
 
-export default MovieList;
+const mapStateToProps = (state) => {
+  return {movies: state.movieReducer.movies}
+}
+
+export default connect(mapStateToProps)(MovieList);
